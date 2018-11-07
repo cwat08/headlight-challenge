02d994275c04291d6baaa426a9740982492e8943
Claire Watson <watson.e.claire@gmail.com>
2018-11-07 16:06:55 -0500

    first commit

---
+++ client/components/Main.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Main extends Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return <h1>Hello World!</h1>
+  }
+}
+
+export default Main
